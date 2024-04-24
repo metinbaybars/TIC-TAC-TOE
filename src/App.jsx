0a28d62ffb5e610afb94c053eedd54710d424360
metinbaybars <baybarsfb2000@hotmail.com>
2024-04-24 15:43:23 +0300

    game initialization

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import Player from './components/Player';
+import GameBoard from './components/GameBoard';
+import { useState } from 'react';
+
+
+
+function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+  function handleSelectSquare () {
+    setActivePlayer((curActivePLayer)=> curActivePLayer === 'X' ? 'O': 'X');
+  }
+
+  return (
+    <div className="App">
+      <div id='game-container'>
+        <ol id='players'  className='highlight-player'>
+        <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'} />
+        <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'} />
+        </ol>
+       <GameBoard onSelectSquare={handleSelectSquare} activePLayerSymbol={activePlayer}/>
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
