a73d9802239c2807a1f2b17630f7038e4cc36cc5
metinbaybars <baybarsfb2000@hotmail.com>
2024-05-04 18:06:45 +0300

    gameOVer feauture is added, over selection bug is fixed, restart option is added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,31 +1,34 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function Player( {initialName, symbol, isActive}) {
-    const [playerName, setPlayerName] = useState()
-    const [isEditing, setIsEditing] = useState(false);
-    
-    function handleEditClick () {
-        setIsEditing((editing) => !editing  ) ;
-    }
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleChange (event) {
-        setPlayerName(event.target.value);
-    }
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
 
-    let editablePlayerName =<span className='player-name'> {playerName}</span>
-   
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-    if (isEditing) {
-     editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
-    }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
 
-    return (
-        <li className= {isActive ? 'active': undefined}> 
-        <span className='player'>
-            {editablePlayerName}
-         <span className='player-symbol'> {symbol} </span>
-        </span>
-        <button onClick={handleEditClick}> {isEditing ?  'Save' : ' Edit'} </button>
-      </li>
-    )
-};
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = 'Save';
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
